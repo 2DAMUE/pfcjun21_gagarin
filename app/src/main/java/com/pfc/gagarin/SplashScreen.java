d0fc6fb66725ec1c2e6257e3e25d984e87c7e196
fabioladecena <fabidecena@gmail.com>
2021-05-18 14:46:50 +0200

    splash

---
+++ app/src/main/java/com/pfc/gagarin/SplashScreen.java
@@ -0,0 +1,47 @@
+package com.pfc.gagarin;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    //Variables
+    ImageView logomars;
+    Animation marsAnimation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Status Bar off
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.splashscreen);
+
+        openApp(true);
+
+        logomars = findViewById(R.id.homeis_logo);
+        marsAnimation = AnimationUtils.loadAnimation(this, R.anim.hyperspace_jump);
+        logomars.startAnimation(marsAnimation);
+    }
+
+    private void openApp(boolean locationPermission) {
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen
+                        .this, MainActivity.class);
+                startActivity(intent);
+            }
+        }, 4000);
+    }
+}

20c741c312894391259c0b362a7b7e5903f94542
Ionutalin23 <ionutalin64@gmail.com>
2021-05-26 12:02:28 +0200

    Diseño ventana Noticias en progreso, version 1

--- app/src/main/java/com/pfc/gagarin/NoticiaScreen.java
+++ app/src/main/java/com/pfc/gagarin/NoticiaScreen.java
@@ -2,13 +2,52 @@ package com.pfc.gagarin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+
+import com.eightbitlab.supportrenderscriptblur.SupportRenderScriptBlur;
+
+import eightbitlab.com.blurview.BlurView;
 
 public class NoticiaScreen extends AppCompatActivity {
+    private ViewGroup root_story;
+    private BlurView blur_scroll_story;
+    private BlurView blur_blu_story;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_noticia);
+        getSupportActionBar().hide();
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        root_story= findViewById(R.id.root_story);
+        blur_scroll_story = findViewById(R.id.blur_scroll_story);
+        blur_blu_story = findViewById(R.id.blur_blu_story);
+
+        blurBackground();
+
     }
+
+    //Desenfocado imagen
+
+    private void blurBackground() {
+        final float radius = 1f;
+        final Drawable windowBackground = getWindow().getDecorView().getBackground();
+        /*
+        blur_scroll_story.setupWith(root_story)
+                .setFrameClearDrawable(windowBackground)
+                .setBlurAlgorithm(new SupportRenderScriptBlur(this))
+                .setBlurRadius(radius)
+                .setHasFixedTransformationMatrix(true);
+         */
+        blur_blu_story.setupWith(root_story)
+                .setFrameClearDrawable(windowBackground)
+                .setBlurAlgorithm(new SupportRenderScriptBlur(this))
+                .setBlurRadius(radius)
+                .setHasFixedTransformationMatrix(true);
+    }
+
+
 }
